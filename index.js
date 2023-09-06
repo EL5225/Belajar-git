bcd7dea0e2793653cd9ed573f25cf09b2cf864ae
EL5225 <luthfiramadhan155@gmail.com>
2023-09-06 21:44:20 +0700

    initial files

---
+++ index.js
@@ -0,0 +1,12 @@
+class Human {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  introduce() {
+    console.log(
+      `Hello, my name is ${this.name} and I am ${this.age} years old.`
+    );
+  }
+}
